25ee12a8ecb408ebed92922623b28d44d2038a52
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-01-20 00:29:45 +0600

    frontend ongoing

--- resources/views/read-more.blade.php
+++ resources/views/read-more.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Blog - Home</title>
+    <title>Blog Post - Read More</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -11,14 +11,14 @@
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="#">My Blog</a>
+            <a class="navbar-brand" href="index.html">My Blog</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
+                        <a class="nav-link" href="index.html">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
@@ -31,29 +31,36 @@
         </div>
     </nav>
 
-    <!-- Main Content -->
+    <!-- Blog Content -->
     <div class="container mt-5">
         <div class="row">
-            <!-- Blog Posts -->
             <div class="col-lg-8">
-                <div class="card mb-4">
-                    <img src="https://via.placeholder.com/750x300" class="card-img-top" alt="Post Thumbnail">
-                    <div class="card-body">
-                        <h5 class="card-title">Blog Post Title</h5>
-                        <p class="card-text">This is a short description of the blog post content.</p>
-                        <a href="read-more.html" class="btn btn-primary">Read More</a>
+                <h1 class="mb-4">Blog Post Title</h1>
+                <img src="https://via.placeholder.com/750x300" class="img-fluid mb-4" alt="Post Thumbnail">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.</p>
+                <hr>
+                
+                <!-- Comments Section -->
+                <div id="comments" class="mt-5">
+                    <h3>Comments</h3>
+                    <!-- Add Comment -->
+                    <div class="mb-4">
+                        <h5>Add a Comment</h5>
+                        <textarea class="form-control mb-2" rows="3" placeholder="Write your comment..."></textarea>
+                        <button class="btn btn-primary">Post Comment</button>
                     </div>
-                </div>
-            </div>
+                    <hr>
 
-            <!-- Sidebar -->
-            <div class="col-lg-4">
-                <div class="card mb-4">
-                    <div class="card-header">Search</div>
-                    <div class="card-body">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search for...">
-                            <button class="btn btn-primary" type="button">Go!</button>
+                    <!-- Display Comments -->
+                    <div class="comment">
+                        <p><strong>User1:</strong> This is a great post! Thank you for sharing.</p>
+                        <!-- Reply -->
+                        <textarea class="form-control mb-2" rows="2" placeholder="Reply to this comment..."></textarea>
+                        <button class="btn btn-secondary btn-sm">Reply</button>
+                        <hr>
+                        <!-- Nested Comment -->
+                        <div class="ms-4">
+                            <p><strong>User2:</strong> I completely agree with you!</p>
                         </div>
                     </div>
                 </div>
