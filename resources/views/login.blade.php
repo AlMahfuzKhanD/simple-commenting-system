72054cf40e730159bedc3b47e5e1359c8457cd42
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-01-20 18:36:10 +0600

    login page design done

---
+++ resources/views/login.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Common Head -->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="Jassa">
+	<meta name="keywords" content="Jassa, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="shortcut icon" href="{{asset('asset/backend/img/icons/icon-48x48.png')}}">
+
+
+	<title>Agri Stock</title>
+
+	<link href="{{asset('asset/backend/css2.css?family=Inter:wght@300;400;600&display=swap')}}" rel="stylesheet">
+
+	<link class="js-stylesheet" href="{{ asset('asset/backend/css/light.css')}}" rel="stylesheet">
+	<!-- Toastr CSS -->
+	@include('common.common-css')
+	<!-- Toastr CSS -->
+
+	<style>
+		body {
+			opacity: 0;
+		}
+	</style>
+	<!-- END SETTINGS -->
+</head>
+<!-- End Common Head -->
+
+
+<body data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-layout="default">
+	<main class="d-flex w-100 h-100">
+		<div class="container d-flex flex-column">
+			<div class="row vh-100">
+				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+					<div class="d-table-cell align-middle">
+
+						<div class="text-center mt-4">
+							<h1 class="h2">Welcome back</h1>
+							<p class="lead">
+								Sign in to your account to get access
+							</p>
+						</div>
+
+						<div class="card">
+							<div class="card-body">
+								<div class="m-sm-4">
+									<form method="POST" action="{{ route('auth.login')}}">
+                                        @csrf
+										<div class="mb-3">
+											<label class="form-label">Email</label>
+											<input class="form-control form-control-lg" type="email" name="email" placeholder="Enter your email">
+										</div>
+										<div class="mb-3">
+											<label class="form-label">Password</label>
+											<input class="form-control form-control-lg" type="password" name="password" placeholder="Enter your password">
+
+										</div>
+										<div class="text-center mt-3">
+											<button type="submit" class="btn btn-lg btn-primary">Sign in</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<script src="{{ asset('asset/backend/js/app.js')}}"></script> 
+	@include('common.common-js')
+</body>
+
+</html>
